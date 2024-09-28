ca4ff11ff3ae3c165d0af80bb138c01d06d0b32d
sunwei <sw20002000@163.com>
2024-09-28 13:16:30 +0800

    1

--- spp.cpp
+++ spp.cpp
@@ -1,22 +1,16 @@
 #include "include/Harem.h"
-#include "include/Matrix.h"
 #include <cstdio>
 #include <cstring>
 #include <iostream>
 using namespace std;
 
 int main() {
-  // Harem *girl = new Harem(18, 165, 105);
-  // bool check = girl->check_beauty();
-  // if (check)
-  //   cout << "nice girl" << endl;
-  // else
-  //   cout << "sad girl" << endl;
-  // girl->show();
-
-  Matrix *matrix = new Matrix(4);
-  matrix->InitMatrix();
-  matrix->assignMatrix();
-  matrix->show();
+  Harem *girl = new Harem(18, 165, 105);
+  bool check = girl->check_beauty();
+  if (check)
+    cout << "nice girl" << endl;
+  else
+    cout << "sad girl" << endl;
+  girl->show();
   return 0;
 }
